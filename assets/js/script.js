3a441543c69e2502f56ed746a37e7ce28d9b6992
Melvin Charlotte <melvin.charlotte24@gmail.com>
2022-01-10 11:49:44 +0100

    ajout de l'animation de la navBar au scroll

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,3 +13,19 @@ closeBtn.addEventListener("click", function(){
     navMenu.style.display = "none";
     closeBtn.style.display = "none";
 })
+
+//ANIMATION NAVBAR
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    let textNav = document.querySelector("#navText");
+    let navBar = document.getElementById("navBar");
+
+  if (document.body.scrollTop > 700 || document.documentElement.scrollTop > 700) {
+    navBar.style.padding = "0.5em 0";
+    textNav.style.fontSize = "18px";
+  } else {
+    navBar.style.padding = "1.5em 0";
+    textNav.style.fontSize = "24px";
+  }
+}
