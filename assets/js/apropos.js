b1921f060ed12d03dd7474e0c3fe5c587a42ed99
Aurélie Manneville <aurelmann79@gmail.com>
2022-01-12 09:01:32 +0100

    MAJ branch

---
+++ assets/js/apropos.js
@@ -0,0 +1,16 @@
+    function closeImg(){
+        document.querySelector('.modal').classList.toggle('displayNone');
+    };
+    
+    
+    
+    function openModal(idModal){
+        var modals = document.querySelectorAll(".displayNone");
+        var modalsLenght = modals.length;
+    
+        for (let i = 0; i < modalsLenght; i++){
+            modals[i].classList.add('displayNone');
+        }
+        document.getElementById(idModal).classList.toggle('displayNone');
+    };
+    
